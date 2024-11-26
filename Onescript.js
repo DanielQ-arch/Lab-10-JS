6d5c4a583e70c322f2f453c7a914cf7c95bacf14
DanielQ-arch <drq@njit.edu>
2024-11-26 17:48:26 -0500

    update

--- Onescript.js
+++ Onescript.js
@@ -4,4 +4,19 @@ function greetUser(UserName){
 
 greetUser('Harry');
 
-document.getElementById('message').innerText = "Hello, World";
+function updatebutton(){
+    document.getElementById('message').innerText = "Hello, World";
+}
+
+function updatebutton2(){
+   const value1 = document.getElementById('inputfield1').value;
+   const value2 = document.getElementById('inputfield2').value;
+   document.getElementById('result').innerText = value1 + value2;
+}
+
+if 
+
+function updatebutton3(){
+    document.getElementById('title2').innerText = document.getElementById('inputfield3').value
+    
+}
