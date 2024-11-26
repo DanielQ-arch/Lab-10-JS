0a6d741f68015d72954e9cb3a6f6a95051226187
DanielQ-arch <drq@njit.edu>
2024-11-26 08:52:55 -0500

    update

---
+++ Onescript.js
@@ -0,0 +1,7 @@
+function greetUser(UserName){
+    document.getElementById('output').innerText = "Hello," + UserName;
+}
+
+greetUser('Harry');
+
+document.getElementById('message').innerText = "Hello, World";
