2c5c08326aa354a1e5c1dbc34c820a38096cfcee
DanielQ-arch <drq@njit.edu>
2024-11-26 20:39:11 -0500

    update

--- Onescript.js
+++ Onescript.js
@@ -4,19 +4,52 @@ function greetUser(UserName){
 
 greetUser('Harry');
 
+
+
+
+
 function updatebutton(){
     document.getElementById('message').innerText = "Hello, World";
 }
 
-function updatebutton2(){
-   const value1 = document.getElementById('inputfield1').value;
-   const value2 = document.getElementById('inputfield2').value;
-   document.getElementById('result').innerText = value1 + value2;
+
+
+
+
+
+
+const sum=(value1,value2)=>{
+   var value1 = document.getElementById('inputField1').value;
+   var value2 = document.getElementById('inputField2').value;
+   document.getElementById('result').innerText = sum(value1 + value2);
 }
 
 
 
+
+
+
+
+
+
 function updatebutton3(){
-    document.getElementById('title2').innerText = document.getElementById('inputfield3').value;
+    document.getElementById('title2').innerText = document.getElementById('inputField3').value;
     
+}
+
+
+
+
+
+
+function updatebutton4(){
+    var value4 = document.getElementById('inputField4').value;
+    var value5 = document.getElementById('inputField5').value;
+    document.getElementById('combinedText').innerText = value4 + value5;
+}
+
+
+
+function updatebackground(){
+    document.getElementById('colorBox').style.backgroundColor = 'blue';
 }
